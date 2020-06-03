58ad9aa1545b3c6f5e38b9fc8a5b309a63746cda
Erik de Castro Lopo <erikd@mega-nerd.com>
2020-06-03 14:03:27 +1000

    asd

--- schema/migration-1-0003-20200211.sql
+++ schema/migration-1-0003-20200211.sql
@@ -12,7 +12,7 @@ BEGIN
     -- to support Word128. We have defined outsum as a Word128 and the easiest
     -- way to store than in the database as a readable value is using show/read
     -- in the database as a bytea.
-    EXECUTE 'CREATE DOMAIN outsum AS varchar;';
+    EXECUTE 'CREATE DOMAIN outsum AS text;';
 
     UPDATE "schema_version" SET stage_one = next_version;
     RAISE NOTICE 'DB has been migrated to stage_one version %', next_version;
