f366dbe6fdb5872396a6576580af00d6cdb8061b
Cmdv <vincent@cmdv.me>
2023-04-04 15:59:36 +0100

    add a check to see if contraint exists

--- cardano-db/src/Cardano/Db/AlterTable.hs
+++ cardano-db/src/Cardano/Db/AlterTable.hs
@@ -6,8 +6,8 @@
 {-# OPTIONS_GHC -Wno-unused-local-binds #-}
 
 module Cardano.Db.AlterTable (
-  AlterTable(..),
-  DbAlterTableException(..),
+  AlterTable (..),
+  DbAlterTableException (..),
   alterTable,
 ) where
 
@@ -16,9 +16,9 @@ import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Trans.Control (MonadBaseControl)
 import Control.Monad.Trans.Reader (ReaderT)
 import qualified Data.Text as T
-import Database.Persist.Postgresql (ConstraintNameDB (..), EntityNameDB (..), FieldNameDB (..), SqlBackend, rawExecute, EntityDef, getEntityFields, fieldDB)
-import Database.PostgreSQL.Simple (SqlError (..), ExecStatus (..))
 import Database.Persist.EntityDef.Internal (entityDB)
+import Database.Persist.Postgresql (ConstraintNameDB (..), EntityDef, EntityNameDB (..), FieldNameDB (..), Single (..), SqlBackend, fieldDB, getEntityFields, rawExecute, rawSql)
+import Database.PostgreSQL.Simple (ExecStatus (..), SqlError (..))
 
 -- The ability to `ALTER TABLE` currently dealing with `CONSTRAINT` but can be extended
 data AlterTable
@@ -36,29 +36,48 @@ alterTable ::
   forall m.
   ( MonadBaseControl IO m
   , MonadIO m
+  , MonadFail m
   ) =>
   EntityDef ->
   AlterTable ->
   ReaderT SqlBackend m ()
 alterTable entity (AddUniqueConstraint cname cols) = do
-  -- Check that input fields are indeed present
+  -- Check that entity fields are in the schema
   if checkAllFieldsValid entity cols
-    then handle alterTableExceptHandler (rawExecute query [])
-    else liftIO $ throwIO (DbAlterTableException "Constraint field does not exist" sqlError)
+    then do
+      -- check if the constraint name already exists
+      constraintRes <- queryConstraint cname
+      if constraintRes == "1"
+        then throwErr "Constraint field already exist"
+        else -- if it doesn't exist then add a new constraint
+          handle alterTableExceptHandler (rawExecute queryAddConstraint [])
+    else error "Some of the unique values which you are being added to the constraint don't correlate"
   where
-    query :: T.Text
-    query =
+    queryAddConstraint :: T.Text
+    queryAddConstraint =
       T.concat
         [ "ALTER TABLE "
         , unEntityNameDB (entityDB entity)
-        , " ADD CONSTRAINT IF NOT EXISTS "
+        , " ADD CONSTRAINT "
         , unConstraintNameDB cname
         , " UNIQUE("
         , T.intercalate "," $ map escapeDBName' cols
         , ")"
         ]
+
     escapeDBName' :: FieldNameDB -> T.Text
     escapeDBName' name = unFieldNameDB name
+
+    throwErr :: forall m'. (MonadIO m') => [Char] -> ReaderT SqlBackend m' ()
+    throwErr e = liftIO $ throwIO (DbAlterTableException e sqlError)
+
+    queryCheckConstraint :: T.Text
+    queryCheckConstraint =
+      T.concat
+        [ "SELECT COUNT(*) FROM pg_constraint WHERE conname ='"
+        , unConstraintNameDB cname
+        , "'"
+        ]
 alterTable entity (DropUniqueConstraint cname) =
   handle alterTableExceptHandler (rawExecute query [])
   where
@@ -70,6 +89,18 @@ alterTable entity (DropUniqueConstraint cname) =
         , " DROP CONSTRAINT IF EXISTS "
         , unConstraintNameDB cname
         ]
+
+-- check if a constraint is already present
+queryConstraint ::
+  ( MonadIO m
+  , MonadFail m
+  ) =>
+  ConstraintNameDB ->
+  ReaderT SqlBackend m T.Text
+queryConstraint cname = do
+  let query = T.concat ["SELECT 1 FROM pg_constraint WHERE conname ='", unConstraintNameDB cname, "'"]
+  [Single constraintNum] <- rawSql query []
+  pure constraintNum
 
 -- check to see that the field inputs exist
 checkAllFieldsValid :: Foldable t => EntityDef -> t FieldNameDB -> Bool
@@ -86,10 +117,11 @@ alterTableExceptHandler ::
 alterTableExceptHandler e = liftIO $ throwIO (DbAlterTableException "" e)
 
 sqlError :: SqlError
-sqlError = SqlError
-  { sqlState = ""
-  , sqlExecStatus = FatalError
-  , sqlErrorMsg = ""
-  , sqlErrorDetail = ""
-  , sqlErrorHint = ""
-  }
+sqlError =
+  SqlError
+    { sqlState = ""
+    , sqlExecStatus = FatalError
+    , sqlErrorMsg = ""
+    , sqlErrorDetail = ""
+    , sqlErrorHint = ""
+    }
